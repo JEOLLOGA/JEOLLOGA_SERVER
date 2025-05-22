3191f3be2ed8bb213d146d98b2ab373ce40abfb9
dajinYang <didekwls0104@naver.com>
2025-05-23 03:45:38 +0900

    [feat] filter 도메인 생성

---
+++ src/main/java/sopt/jeolloga/filter/core/FilterService.java
@@ -0,0 +1,12 @@
+package sopt.jeolloga.filter.core;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FilterService {
+    private final FilterRepository filterRepository;
+
+    public FilterService(FilterRepository filterRepository) {
+        this.filterRepository = filterRepository;
+    }
+}
