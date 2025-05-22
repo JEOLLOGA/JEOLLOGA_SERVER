9d3470e99a843f7547d8a873e6499e06a8864610
dajinYang <didekwls0104@naver.com>
2025-05-23 02:51:00 +0900

    [feat] 클라이언트 요청 오류 처리

---
+++ src/main/java/sopt/jeolloga/exception/BusinessException.java
@@ -0,0 +1,13 @@
+package sopt.jeolloga.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMsg());
+        this.errorCode = errorCode;
+    }
+}
