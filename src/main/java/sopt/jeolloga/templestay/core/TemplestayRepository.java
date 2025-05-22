b06564c03527bb67a5297f26db041b9cab3ad620
dajinYang <didekwls0104@naver.com>
2025-05-23 03:40:17 +0900

    [feat] templestay 어노테이션 추가

--- src/main/java/sopt/jeolloga/templestay/core/TemplestayRepository.java
+++ src/main/java/sopt/jeolloga/templestay/core/TemplestayRepository.java
@@ -1,8 +1,10 @@
 package sopt.jeolloga.templestay.core;
 
+import jdk.jfr.Registered;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sopt.jeolloga.templestay.Templestay;
 
+@Registered
 public interface TemplestayRepository extends JpaRepository<Templestay, Long> {
 
 }
