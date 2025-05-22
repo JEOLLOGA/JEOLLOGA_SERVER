566c84f083477da40372231b82302ec0b7095bfe
dajinYang <didekwls0104@naver.com>
2025-05-23 02:31:21 +0900

    [feat] 공통 응답 생성

---
+++ src/main/java/sopt/jeolloga/dto/ApiResponse.java
@@ -0,0 +1,19 @@
+package sopt.jeolloga.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.http.HttpStatus;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ApiResponse<T>(
+        int code,
+        String msg,
+        T data
+) {
+    public static <T> ApiResponse<T> success(T data, String msg) {
+        return new ApiResponse(HttpStatus.OK.value(), msg, data);
+    }
+
+    public static <T> ApiResponse success(T data) {
+        return success(data, "응답 성공");
+    }
+}
