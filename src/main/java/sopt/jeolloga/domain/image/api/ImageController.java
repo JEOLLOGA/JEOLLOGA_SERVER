37d94e42bf5a010d7674d296f3c2cdffc32048ac
dajinYang <didekwls0104@naver.com>
2025-05-24 16:58:56 +0900

    [fix] 요청 url수정

--- src/main/java/sopt/jeolloga/domain/image/api/ImageController.java
+++ src/main/java/sopt/jeolloga/domain/image/api/ImageController.java
@@ -10,7 +10,7 @@ import sopt.jeolloga.domain.image.api.dto.TemplestayImgRes;
 import sopt.jeolloga.domain.image.core.ImageService;
 
 @RestController
-@RequestMapping("/templestay")
+@RequestMapping("/api/templestay")
 public class ImageController {
     private final ImageService imageService;
 
