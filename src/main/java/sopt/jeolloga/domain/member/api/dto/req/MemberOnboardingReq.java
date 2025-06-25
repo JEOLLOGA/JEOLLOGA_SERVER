b572e6cbac41cc60ddd9ddd9ba2ac66e8a6fa740
dajinYang <didekwls0104@naver.com>
2025-06-26 03:27:25 +0900

    [fix] null 체크 어노테이션 변경

--- src/main/java/sopt/jeolloga/domain/member/api/dto/req/MemberOnboardingReq.java
+++ src/main/java/sopt/jeolloga/domain/member/api/dto/req/MemberOnboardingReq.java
@@ -1,11 +1,12 @@
 package sopt.jeolloga.domain.member.api.dto.req;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record MemberOnboardingReq(
         @NotBlank(message = "나이대는 필수입니다.") String ageRange,
         @NotBlank(message = "성별은 필수입니다.") String gender,
         @NotBlank(message = "종교 정보는 필수입니다.") String religion,
-        @NotBlank(message = "템플스테이 경험 여부는 필수입니다.") boolean hasExperience
+        @NotNull(message = "템플스테이 경험 여부는 필수입니다.") boolean hasExperience
 ) {
 }
