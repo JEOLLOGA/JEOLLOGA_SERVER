c004b15b655d264cce538dc005985bbf8edc3016
dajinYang <didekwls0104@naver.com>
2025-06-30 17:44:21 +0900

    [feat] 위시리스트 담기

--- src/main/java/sopt/jeolloga/domain/wishlist/api/WishlistController.java
+++ src/main/java/sopt/jeolloga/domain/wishlist/api/WishlistController.java
@@ -1,13 +1,38 @@
 package sopt.jeolloga.domain.wishlist.api;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sopt.jeolloga.common.dto.ApiResponse;
+import sopt.jeolloga.domain.auth.jwt.JwtCookieProvider;
+import sopt.jeolloga.domain.auth.jwt.JwtTokenGenerator;
 import sopt.jeolloga.domain.wishlist.core.WishlistService;
 
 @RestController
+@RequestMapping("/user")
 public class WishlistController {
     private final WishlistService wishlistService;
+    private final JwtCookieProvider jwtCookieProvider;
+    private final JwtTokenGenerator jwtTokenGenerator;
 
-    public WishlistController(WishlistService wishlistService) {
+    public WishlistController(WishlistService wishlistService, JwtCookieProvider jwtCookieProvider, JwtTokenGenerator jwtTokenGenerator) {
         this.wishlistService = wishlistService;
+        this.jwtCookieProvider = jwtCookieProvider;
+        this.jwtTokenGenerator = jwtTokenGenerator;
+    }
+
+    @PostMapping("/wish/{id}")
+    public ResponseEntity<ApiResponse<?>> updateWishlist(
+            HttpServletRequest request,
+            @PathVariable Long id
+    ) {
+        String accessToken = jwtCookieProvider.extractAccessToken(request);
+        Long userId = jwtTokenGenerator.extractUserId(accessToken);
+
+        wishlistService.updateWishlist(userId, id);
+        return ResponseEntity.ok(ApiResponse.success(null));
     }
 }
--- src/main/java/sopt/jeolloga/domain/wishlist/core/WishlistRepository.java
+++ src/main/java/sopt/jeolloga/domain/wishlist/core/WishlistRepository.java
@@ -3,9 +3,12 @@ package sopt.jeolloga.domain.wishlist.core;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import sopt.jeolloga.domain.member.Member;
+import sopt.jeolloga.domain.templestay.Templestay;
 import sopt.jeolloga.domain.wishlist.Wishlist;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
@@ -13,4 +16,5 @@ public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
     List<Long> findTemplestayIdsByMemberId(Long memberId);
     boolean existsByMember_IdAndTemplestay_Id(Long memberId, Long templestayId);
 
+    Optional<Wishlist> findByMemberAndTemplestay(Member member, Templestay templestay);
 }
