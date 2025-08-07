926d27e2bafa933a214591069cf38a0b8b30a065
dajinYang <didekwls0104@naver.com>
2025-08-07 21:37:06 +0900

    [refactor] enum값 label로 변경

--- src/main/java/sopt/jeolloga/common/filter/Activity.java
+++ src/main/java/sopt/jeolloga/common/filter/Activity.java
@@ -1,16 +1,38 @@
 package sopt.jeolloga.common.filter;
 
+import sopt.jeolloga.exception.BusinessErrorCode;
+import sopt.jeolloga.exception.BusinessException;
+
 public enum Activity implements BitMask {
-    _108배(0),
-    스님과의차담(1),
-    새벽예불(2),
-    염주만들기(3),
-    연등만들기(4),
-    명상(5);
+    _108배(0, "108배"),
+    스님과의차담(1, "스님과의 차담"),
+    새벽예불(2, "새벽 예불"),
+    염주만들기(3, "염주 만들기"),
+    연등만들기(4, "연등 만들기"),
+    명상(5, "명상");
 
     private final int bit;
-    Activity(int bit) { this.bit = bit; }
+    private final String label;
 
-    @Override public int getBit() { return bit; }
-    @Override public String getLabel() { return name(); }
+    Activity(int bit, String label) {
+        this.bit = bit;
+        this.label = label;
+    }
+
+    @Override
+    public int getBit() {
+        return bit;
+    }
+
+    @Override
+    public String getLabel() {
+        return label;
+    }
+
+    public static Activity fromLabel(String label) {
+        for (Activity a : values()) {
+            if (a.label.equals(label)) return a;
+        }
+        throw new BusinessException(BusinessErrorCode.BAD_REQUEST_ENUM);
+    }
 }
--- src/main/java/sopt/jeolloga/common/filter/EtcOption.java
+++ src/main/java/sopt/jeolloga/common/filter/EtcOption.java
@@ -1,13 +1,35 @@
 package sopt.jeolloga.common.filter;
 
+import sopt.jeolloga.exception.BusinessErrorCode;
+import sopt.jeolloga.exception.BusinessException;
+
 public enum EtcOption implements BitMask {
-    주차가능(0),
-    _1인실(1),
-    단체가능(2);
+    주차가능(0, "주차 가능"),
+    _1인실(1, "1인실"),
+    단체가능(2, "단체 가능");
 
     private final int bit;
-    EtcOption(int bit) { this.bit = bit; }
+    private final String label;
 
-    @Override public int getBit() { return bit; }
-    @Override public String getLabel() { return name(); }
+    EtcOption(int bit, String label) {
+        this.bit = bit;
+        this.label = label;
+    }
+
+    @Override
+    public int getBit() {
+        return bit;
+    }
+
+    @Override
+    public String getLabel() {
+        return label;
+    }
+
+    public static EtcOption fromLabel(String label) {
+        for (EtcOption e : values()) {
+            if (e.label.equals(label)) return e;
+        }
+        throw new BusinessException(BusinessErrorCode.BAD_REQUEST_ENUM);
+    }
 }
