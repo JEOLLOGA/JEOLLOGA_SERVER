c4ae13c6d15d8710fc6abe462939444a0d731453
YangDaJin0104 <didekwls0104@naver.com>
2026-06-01 21:04:03 +0900

    [chore] 인기 템플스테이

--- src/main/java/sopt/jeolloga/domain/templestay/core/TemplestayService.java
+++ src/main/java/sopt/jeolloga/domain/templestay/core/TemplestayService.java
@@ -33,7 +33,7 @@ import static sopt.jeolloga.util.FilterMaskUtil.decodeMask;
 @RequiredArgsConstructor
 public class TemplestayService {
 
-    private static final List<Long> RECOMMEND_TEMPLATESTAY_IDS = List.of(1561L, 1489L, 1458L);
+    private static final List<Long> RECOMMEND_TEMPLATESTAY_IDS = List.of(1561L, 1576L, 1506L);
 
     private final TemplestayRepository templestayRepository;
     private final ImageRepository imageRepository;
